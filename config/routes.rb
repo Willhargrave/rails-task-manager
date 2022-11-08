1de18da3750ff021f4cc25d81cfaa8df48fef0fc
Will Hargrave willhargrave@hotmail.co.uk <willhargrave@hotmail.co.uk>
2022-11-08 16:58:53 +0900

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,15 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  get 'tasks', to: 'tasks#index', as: :tasks
   # root "articles#index"
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+
+  get 'task/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'task/:id', to: 'tasks#update'
+
+  delete 'task/:id', to: 'tasks#destroy'
+  get 'task/:id', to: 'tasks#show', as: :task
+
 end
